b17a8200b0a2cd4d25c7934b4c8cc7c078220e59
hammdata <gohammdata@gmail.com>
2020-03-17 17:15:37 -0400

    Detail books (list and delete functions) added

--- src/app/books/detail/detail.component.ts
+++ src/app/books/detail/detail.component.ts
@@ -1,4 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Apollo, QueryRef } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Book } from '../book';
+
+
+const bookQuery = gql`
+  query book($bookId: String) {
+    book(id: $bookId) {
+      _id
+      isbn
+      title
+      author
+      description
+      published_year
+      publisher
+      updated_date
+    }
+  }
+`;
+
+const deleteBook = gql`
+  mutation removeBook($id: String!) {
+    removeBook(id:$id) {
+      _id
+    }
+  }
+`;
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +35,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  book: Book = { id: '', isbn: '', title: '',
+                author: '', description: '', publisher: '',
+                publishedYear: null, updatedDate: null };
 
-  ngOnInit(): void {
+  isLoadingResults = true;
+  resp: any = {};
+
+private query: QueryRef<any>;
+
+  constructor(private apollo: Apollo, private router: Router, private route: ActivatedRoute) {
+    getBookDetails() {
+        const id = this.route.snapshot.params.id;
+        this.query = this.apollo.watchQuery({
+        query: bookQuery,
+        variables: { bookId: id }
+  });
+
+  this.query.valueChanges.subscribe(res => {
+    this.book = res.data.book;
+    console.log(this.book);
+    this.isLoadingResults = false;
+  });
   }
+}
 
+  ngOnInit() {
+    this.getBookDetails();
+  }
+  
+  deleteBook() {
+    this.isLoadingResults = true;
+    const bookId = this.route.snapshot.params.id;
+    this.apollo.mutate({
+      mutation: deleteBook,
+      variables: {
+        id: bookId
+      }
+    }).subscribe(({ data }) => {
+      console.log('got data', data);
+      this.isLoadingResults = false;
+      this.router.navigate(['/books']);
+    }, (error) => {
+      console.log('there was an error sending the query', error);
+      this.isLoadingResults = false;
+    });
+  }
 }
