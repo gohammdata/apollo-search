831728defb2026116e0a7e4bf50a1be10ca7abcd
hammdata <gohammdata@gmail.com>
2020-03-17 16:57:20 -0400

    Setting up connection to the GraphQL in the Express.js server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeesComponent } from './employees/employees.component';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,23 @@ import { EmployeesComponent } from './employees/employees.component';
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({ url: 'http://localhost:8080/graphql'}),
+      cache: new InMemoryCache()
+    });
+  }
+
+ }
